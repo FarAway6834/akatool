0ff9bd77713a7e82c5ad574e316a4939fbb99efd
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-02-28 11:52:43 +0900

    __init__.py 업데이트

--- akatool/__init__.py
+++ akatool/__init__.py
@@ -1,9 +1,3 @@
-import builtins as __builtin__
-from sympy import abc as __vars__
-import lbdc
-import edprompt
-import clro
-from subpr.lib import __subpr__
-__builtin__.v = __vars__
-__builtin__.incognito = lambda x : __subpr__(f'start chrome --incognito {x}')
-__all__, __version__ = ['__main__', 'confapp'], '0.0.5'
+import akatoollite
+
+__all__, __version__ = ['__main__', 'confapp'], '0.0.6'
